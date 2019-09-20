4019a2492ee15888e294ff9bb9ad86c7923649b8
Khalem <33363455+Khalem@users.noreply.github.com>
2019-09-20 18:48:52 +0100

    Updates

---
+++ precache-manifest.0a35ca1efafe813b004d1e8d425ca9fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab228a8434548898132ec6113daf1d89",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "a78b87e2f44c17b6feae",
+    "url": "/react-calculator/static/css/main.3434edcc.chunk.css"
+  },
+  {
+    "revision": "1c5ee3dff7cb029445e5",
+    "url": "/react-calculator/static/js/2.299ed2ea.chunk.js"
+  },
+  {
+    "revision": "a78b87e2f44c17b6feae",
+    "url": "/react-calculator/static/js/main.ea4ab825.chunk.js"
+  },
+  {
+    "revision": "4dce6e586341e9dff92c",
+    "url": "/react-calculator/static/js/runtime~main.2fce0c4e.js"
+  }
+]);
